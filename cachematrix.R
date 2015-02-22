18a2f77064b800bc1fe94cbe36ac0be924859470
xtianbadillo <correo@christianbadillo.com>
2015-02-22 16:53:27 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-
+# this function works like a class, it creates a list
+# that contains 4 member functions: set, get, setsolve, getsolve
 makeCacheMatrix <- function(x = matrix()) {
   inverso <- NULL # SEtting to null for a future value
   set <- function(y) { #define vector
@@ -13,6 +13,11 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x #returns vector
   setsolve <- function(solve) inverso <<- solve
   getsolve <- function() inverso
+      # return a list that contains these functions, so that we can use
+      # makeCacheMatrix object like these
+      # x <- makeCacheMatrix(testmatrix)
+      # setsolve(newmatrix) # to change matrix
+      # getsolve # to get the setted matrix
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
@@ -24,6 +29,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+ # null if uncalculed
   inverso <- x$getsolve()
   if(!is.null(inverso)) {
     message("getting cached data")
@@ -36,6 +42,8 @@ cacheSolve <- function(x, ...) {
 }
 
 ##Prove
+# probe
+# to generate a matrix
 
 testing <- matrix(rnorm(16,100,10),4,4)
 Cache <- makeCacheMatrix(testing)
